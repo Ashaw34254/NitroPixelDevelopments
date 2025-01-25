953e9c66aed90ad923cfe1dd397edacb70da8ff2
A Shaw <antoniobailey1231@outlook.com>
2025-01-25 18:55:06 +1300

    Please enter the commit message for your changes.  Lines starting  with '' will be ignored, and an empty message aborts t  he commit.
     On branch main
     Your branch is up to date with 'origin/main'.

--- database/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-return [
-    'mongodb_uri' => 'mongodb+srv://thegta5gamerpsn:<db_password>@backroad.2w4x1.mongodb.net/?retryWrites=true&w=majority&appName=backroad'
-];
--- database/vehicles.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE vehicles (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255),
-    category VARCHAR(100),
-    image_path VARCHAR(255),
-    specs TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
